46c8e9452b2bbf6e4603a90e4bd2b72b1a762d99
mttinker <ttinker@nhydra.com>
2020-01-22 21:24:38 -0400

    Load "Results.rdata" to plot results

--- HS_estTAC.r
+++ HS_estTAC.r
@@ -1,6 +1,6 @@
 # Script to plot some results from model fitting
 # Load results file (if not already loaded into workspace):
-load(file="FitHSmod_Results.rdata")
+load(file="Results.rdata")
 #
 require(parallel)
 require(gtools)
